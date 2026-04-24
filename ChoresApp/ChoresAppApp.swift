ce5a5f33e949c6dfa13e354a36ca5fce6a45c298
simrubin <149342852+simrubin@users.noreply.github.com>
2026-04-24 14:10:35 +1000

    Initial Commit

---
+++ ChoresApp/ChoresAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChoresAppApp.swift
+//  ChoresApp
+//
+//  Created by Simeon Rubin on 24/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct ChoresAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChoresApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChoresApp
+//
+//  Created by Simeon Rubin on 24/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
